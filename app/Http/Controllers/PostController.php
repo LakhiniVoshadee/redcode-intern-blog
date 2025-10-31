134c3b29aec30f5cedb63f90add95dfd13df4f63
Lakhini Voshadee Abeysundara <lakhinivoshadee24@gmail.com>
2025-10-31 14:13:43 +0530

    create post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::all();
+        return view('posts.index', ['posts' => $posts]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+
+        Post::create($request->all());
+        return redirect()->route('posts.index')->with('success', 'Post created!');
+    }
 }
